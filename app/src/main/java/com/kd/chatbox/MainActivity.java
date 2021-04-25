af5a490fa3cda316fa753f36b37a0b0dcf75078c
codecontext <kri.i7teen@gmail.com>
2021-04-25 12:53:11 +0800

    Created Chat Window and Menu itesms
    - Created Recycler view for the conversations
    - Created Menu items for Chats, Statuses and Calls
    - Created Top menu items for Settings Groups etc.

--- app/src/main/java/com/kd/chatbox/MainActivity.java
+++ app/src/main/java/com/kd/chatbox/MainActivity.java
@@ -1,14 +1,43 @@
 package com.kd.chatbox;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.kd.chatbox.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.search:
+                Toast.makeText(this, "Search Clicked", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.settings:
+                Toast.makeText(this, "Settings Clicked", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.topmenu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
---
+++ app/src/main/java/com/kd/chatbox/UsersAdapter.java
@@ -0,0 +1,55 @@
+package com.kd.chatbox;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.kd.chatbox.databinding.RowConversationBinding;
+
+import java.util.ArrayList;
+
+public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.UsersViewHolder>{
+
+    Context context;
+    ArrayList<User> users;
+
+    public UsersAdapter(Context context, ArrayList<User> users){
+        this.context = context;
+        this.users = users;
+    }
+
+    @NonNull
+    @Override
+    public UsersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.row_conversation, parent, false);
+
+        return new UsersViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UsersViewHolder holder, int position) {
+        User user = users.get(position);
+
+        holder.binding.userName.setText(user.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    public class UsersViewHolder extends RecyclerView.ViewHolder {
+
+        RowConversationBinding binding;
+
+        public UsersViewHolder(@NonNull View itemView) {
+            super(itemView);
+            binding = RowConversationBinding.bind(itemView);
+        }
+    }
+}
+
