aed90882329d82593749ad7e88de76269632354e
codecontext <kri.i7teen@gmail.com>
2021-05-01 19:48:30 +0800

    Added Status View on top of Chat Window
    - Added CircularStatusView layout for statuses
    - Added logic to upload image in the status
    - Refined the Chat List window

--- app/src/main/java/com/kd/chatbox/Activities/MainActivity.java
+++ app/src/main/java/com/kd/chatbox/Activities/MainActivity.java
@@ -1,23 +1,42 @@
 package com.kd.chatbox.Activities;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.kd.chatbox.Adapters.TopStatusAdapter;
+import com.kd.chatbox.Models.Status;
+import com.kd.chatbox.Models.UserStatus;
 import com.kd.chatbox.R;
 import com.kd.chatbox.Models.User;
 import com.kd.chatbox.Adapters.UsersAdapter;
 import com.kd.chatbox.databinding.ActivityMainBinding;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +44,11 @@ public class MainActivity extends AppCompatActivity {
     FirebaseDatabase database;
     ArrayList<User> users;
     UsersAdapter usersAdapter;
+    TopStatusAdapter statusAdapter;
+    ArrayList<UserStatus> userStatuses;
+    ProgressDialog dialog;
+
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +56,35 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        dialog = new ProgressDialog(this);
+        dialog.setMessage("Uploading Status Image...");
+        dialog.setCancelable(false);
+
         database = FirebaseDatabase.getInstance();
         users = new ArrayList<>();
+        userStatuses = new ArrayList<>();
+
+        database.getReference().child("users").child(FirebaseAuth.getInstance().getUid())
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        user = snapshot.getValue(User.class);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
 
         usersAdapter = new UsersAdapter(this, users);
+        statusAdapter = new TopStatusAdapter(this, userStatuses);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(RecyclerView.HORIZONTAL);
+        binding.statusList.setLayoutManager(layoutManager);
+
+        binding.statusList.setAdapter(statusAdapter);
         binding.recylerView.setAdapter(usersAdapter);
 
         database.getReference().child("users").addValueEventListener(new ValueEventListener() {
@@ -45,7 +94,11 @@ public class MainActivity extends AppCompatActivity {
 
                 for(DataSnapshot snapshot1: snapshot.getChildren()){
                     User user = snapshot1.getValue(User.class);
-                    users.add(user);
+
+                    /* Avoid adding the logged in user to the chat list window */
+                    if(!user.getUid().equals(FirebaseAuth.getInstance().getUid())) {
+                        users.add(user);
+                    }
                 }
 
                 usersAdapter.notifyDataSetChanged();
@@ -56,6 +109,110 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        database.getReference().child("stories").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    userStatuses.clear();
+
+                    for(DataSnapshot storySnapshot : snapshot.getChildren()){
+                        UserStatus status = new UserStatus();
+                        status.setName(storySnapshot.child("name").getValue(String.class));
+                        status.setProfileImage(storySnapshot.child("profileImage").getValue(String.class));
+                        status.setLastUpdated(storySnapshot.child("lastUpdated").getValue(Long.class));
+
+                        ArrayList<Status> statuses = new ArrayList<>();
+
+                        for(DataSnapshot statusSnapshot : storySnapshot.child("statuses").getChildren()){
+                            Status sampleStatus = statusSnapshot.getValue(Status.class);
+                            statuses.add(sampleStatus);
+                        }
+
+                        status.setStatuses(statuses);
+                        userStatuses.add(status);
+                    }
+                    statusAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        binding.bottomNavigationView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.status:
+                    Intent intent = new Intent();
+                    intent.setType("image/*");
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                    startActivityForResult(intent, 75);
+                    break;
+                }
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(data != null){
+            if(data.getData() != null){
+                dialog.show();
+
+                FirebaseStorage storage = FirebaseStorage.getInstance();
+
+                Date date = new Date();
+                StorageReference reference = storage.getReference()
+                        .child("status")
+                        .child(date.getTime() + "");
+
+                reference.putFile(data.getData()).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                        if(task.isSuccessful()){
+                            reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    UserStatus userStatus = new UserStatus();
+                                    userStatus.setName(user.getName());
+                                    userStatus.setProfileImage(user.getProfileImage());
+                                    userStatus.setLastUpdated(date.getTime());
+
+                                    HashMap<String, Object> obj = new HashMap<>();
+                                    obj.put("name", userStatus.getName());
+                                    obj.put("profileImage", userStatus.getProfileImage());
+                                    obj.put("lastUpdated", userStatus.getLastUpdated());
+
+                                    String imageUrl = uri.toString();
+                                    Status status = new Status(imageUrl, userStatus.getLastUpdated());
+
+                                    database.getReference()
+                                            .child("stories")
+                                            .child(FirebaseAuth.getInstance().getUid())
+                                            .updateChildren(obj);
+
+                                    database.getReference()
+                                            .child("stories")
+                                            .child(FirebaseAuth.getInstance().getUid())
+                                            .child("statuses")
+                                            .push()
+                                            .setValue(status);
+
+                                    dialog.dismiss();
+                                }
+                            });
+                        }
+                    }
+                });
+            }
+        }
     }
 
     @Override
